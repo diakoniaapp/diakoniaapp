f41575805e91d8829d79f976d0694a4540968cf6
Diakonia App <telma@diakoniaapp.com.br>
2026-06-12 12:30:53 -0300

    Create Bazar component with basic layout

---
+++ src/pages/bazar/index.tsx
@@ -0,0 +1,11 @@
+export default function Bazar() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bazar / Cantina</h1>
+
+      <p>Módulo funcionando ✅</p>
+
+      <a href="/bazar/caixa">Ir para caixa</a>
+    </div>
+  )
+}
